2e89797a14731fc3779ecff607c71cfb7c1f0b3c
abrarhayat <abrarhayat@gmail.com>
2024-03-19 02:43:33 +1100

    Updated context info for CI/CD for test generation

--- main.py
+++ main.py
@@ -124,6 +124,7 @@ def prompt(filename: str, contents: str, review_type: str) -> str:
             f'Generate a suggested yml file for CI/CD for tests for the file names. Please look at the file names and include a test file for each of the regular source files.'
             f'For Example: It there is a file with filename Util.java, then generate a yml file which would include a test file called UtilTest.java. This will be true for all the source files.'
             f'Please remember that the user will be responsible for adding the test files and pushing them, you should only include the test files to be run in CI/CD. So it will only run the test files in CI/CD.'
+            f'Make sure the yml file runs on Pull Request with types opened and synchronize and with permission of write-all.'
         )
 
 
