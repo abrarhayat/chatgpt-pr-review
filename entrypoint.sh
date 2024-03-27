e6a232495522e9a50d5cea25c3101d4aba3f8286
abrarhayat <abrarhayat@gmail.com>
2024-03-28 05:24:09 +1100

    Fixed order of args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ python /main.py \
 --openai_api_key "$1" \
 --review_type "$2" \
 --anthropic_api_key "$3" \
---github_token "$2" \
---github_pr_id "$3" \
---files "$4" \
---openai_model "$5" \
---openai_temperature "$6" \
+--github_token "$4" \
+--github_pr_id "$5" \
+--files "$6" \
+--openai_model "$7" \
+--openai_temperature "$8" \
 --openai_max_tokens "$9" \
 --logging "$10" \
